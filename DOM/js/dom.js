78621c310177756b3bcc595ad75083f46649f712
Diego <diego.sk75.dgp@gmail.com>
2022-01-21 11:24:51 -0500

    clase 68 69 70

--- DOM/js/dom.js
+++ DOM/js/dom.js
@@ -130,7 +130,7 @@ console.log($linkDOM.dataset.description) */
 
 /* DOM: Texto y HTML ***************************************************************** */
 
-const $whatIsDOM = document.getElementById("que-es")
+/* const $whatIsDOM = document.getElementById("que-es")
 
 let text = `<p>EL modelo de objetos del documento (<b><i>DOM-Document OBject MOdel</i></b>)
 es una API para <documentos HTML y XML/p> 
@@ -141,4 +141,120 @@ codigo JS</p>`;
 $whatIsDOM.innerText = text;//no reconoce etiquetas html
 $whatIsDOM.textContent = text;//no reconoce
 $whatIsDOM.innerHTML = text;//reconoce etiquetas html
-$whatIsDOM.outerHTML = text;//reemplaza el div contenedor
+$whatIsDOM.outerHTML = text;//reemplaza el div contenedor */
+
+/* DOM Traversing: Recorriendo el DOM ***************************************************************** */
+
+/* $cards = document.querySelector(".cards");
+
+console.log($cards);
+console.log($cards.children);
+console.log($cards.children[2]);
+console.log($cards.parentElement);
+console.log($cards.firstChild); // hace referencia al espacio "enter", por lo tanto no es muy útil.
+console.log($cards.firstElementChild); // obtiene el primer elemento de "cards"
+console.log($cards.lastElementChild); // obtiene el último "Elemento" hijo
+console.log($cards.previousElementSibling); // el "Elemento" hermano antes del "card"
+console.log($cards.nextElementSibling); // el siguiente "Elemento" hermano de "card"
+console.log($cards.closest("div")); // es un método que busca el ancestro, el padre más cercano
+console.log($cards.closest("body"));
+console.log($cards.children[3].closest("section")); // el padre mas cercano del elemento n°3 */
+
+/* DOM: Creando Elementos y Fragmentos ***************************************************************** */
+
+/* const $figure = document.createElement("figure"),
+    $img = document.createElement("img"),
+    $figcaption = document.createElement("figcaption"),
+    $figcaptionText = document.createTextNode("Animals"),
+    $cards = document.querySelector(".cards"),
+    $figure2 = document.createElement("figure");
+
+$img.setAttribute("src","https://placeimg.com/200/200/animals");
+$img.setAttribute("alt","Animals");
+$figure.classList.add("card")//agrega estilos
+
+$figcaption.appendChild($figcaptionText);
+$figure.appendChild($img);
+$figure.appendChild($figcaption);
+$cards.appendChild($figure);
+
+$figure2.innerHTML = `<img src="https://placeimg.com/200/200/people" alt="People">
+<figcaption>People</figcaption>`;
+
+$figure2.classList.add("card")
+$cards.appendChild($figure2);
+
+const estaciones =["Primavera","Verano","Otoño","Invierno"],
+$ul = document.createElement("ul");
+
+document.write("<h3>Estaciones del Año</h3>");
+document.body.appendChild($ul);
+
+estaciones.forEach(el => {//por cada elemento
+    //$ul.appendChild(`<li>${el}</li>`);
+    const $li = document.createElement("li");
+    $li.textContent=el;
+    $ul.appendChild($li);
+})
+
+const continentes = ["africa","america","asia","oceania","europa"],
+$ul2 = document.createElement("ul");
+
+document.write("<h3>Continentes del mundo</h3>");
+document.body.appendChild($ul2);
+$ul2.innerHTML = "";
+
+continentes.forEach(el => $ul2.innerHTML += `<li>${el}</li>`);
+
+//Fragmentos
+const meses = [
+    "enero","febrero","marzo","abril","mayo","junio","julio","agosto"
+],
+$ul3 = document.createElement("ul");
+$fragment = document.createDocumentFragment();
+
+meses.forEach(el => {
+    const $li = document.createElement("li");
+    $li.textContent = el;
+    $fragment.appendChild($li);
+});
+
+document.write("<h3>Meses del año</h3>");
+$ul3.appendChild($fragment);
+document.body.appendChild($ul3); */
+
+/* DOM: Templates HTML ***************************************************************** */
+//las etiquetas template no se renderizan en el DOM, se utiliza para generar estructuras dinamicamente
+
+const $cards = document.querySelector(".cards"),
+$template = document.querySelector("#template-card").content,
+$fragment = document.createDocumentFragment(),
+cardContent = [
+    {
+        title:"Tecnologia",
+        img:"https://placeimg.com/200/200/tech",
+    },
+    {
+        title:"Animales",
+        img:"https://placeimg.com/200/200/animals",
+    },
+    {
+        title:"Arquictectura",
+        img:"https://placeimg.com/200/200/arch",
+    },
+    {
+        title:"Gente",
+        img:"https://placeimg.com/200/200/people",
+    }
+];
+
+cardContent.forEach(el => {
+    $template.querySelector("img").setAttribute("src",el.img);
+    $template.querySelector("img").setAttribute("alt",el.title);
+    $template.querySelector("figcaption").textContent = el.title;
+
+    let $clone = document.importNode($template,true);//clonamos el template, toda la estructura
+    $fragment.appendChild($clone);
+});
+
+$cards.appendChild($fragment);
