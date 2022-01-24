55acf2025d0a0d416ca00611ba2cbb0335ad0156
Diego <diego.sk75.dgp@gmail.com>
2022-01-24 11:55:57 -0500

    clase 79

--- DOM/js/dom.js
+++ DOM/js/dom.js
@@ -430,3 +430,20 @@ document.addEventListener("DOMContentLoaded", e => {
     console.log(window.screenY);
 })
 
+/* BOM: Métodos  ***************************************************************** */
+
+const $btnAbrir = document.getElementById("abrir-ventana"),
+$btnCerrar = document.getElementById("cerrar-ventana"),
+$btnImprimir = document.getElementById("imprimir-ventana");
+
+let ventana;
+
+$btnAbrir.addEventListener('click',e => {
+   ventana = window.open("https://jonmircha.com")
+});
+$btnCerrar.addEventListener('click',e => {
+    ventana.close();
+});
+$btnImprimir.addEventListener('click',e => {
+    window.print();//se manda imprimir
+});
