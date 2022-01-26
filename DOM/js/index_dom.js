ac6568df3b02916986a9b19daa73e1567031bcd1
DiegoGutierrezP <diego.sk75.dgp@gmail.com>
2022-01-26 13:41:32 -0500

    clase 87 88

--- DOM/js/index_dom.js
+++ DOM/js/index_dom.js
@@ -1,6 +1,8 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
 import { digitalClock,alarm } from "./reloj.js";
 import { shortcuts,moveBall }from "./teclado.js";
+import countDown from "./cuenta_regresiva.js";
+import scrollTopButton from "./boton_scroll.js";
 
 const d = document;
 
@@ -8,6 +10,8 @@ d.addEventListener("DOMContentLoaded", e=>{//representa la carga del documento
     hamburgerMenu(".panel-btn",".panel",".menu a")
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj")
     alarm("../assets/alarma-guerra.mp3","#activar-alarma","#desactivar-alarma");
+    countDown("countdown","Jan 26, 2022 13:18:19","Feliz CUmpleaños amigo");
+    scrollTopButton(".scroll-top-btn");
 })
 
 d.addEventListener("keydown", e=>{
