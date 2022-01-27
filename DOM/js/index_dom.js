a0ea98a4a4c73532441a08ba42f0212d20b5dada
Diego <diego.sk75.dgp@gmail.com>
2022-01-26 19:55:39 -0500

    clase 89 90

--- DOM/js/index_dom.js
+++ DOM/js/index_dom.js
@@ -3,6 +3,7 @@ import { digitalClock,alarm } from "./reloj.js";
 import { shortcuts,moveBall }from "./teclado.js";
 import countDown from "./cuenta_regresiva.js";
 import scrollTopButton from "./boton_scroll.js";
+import darkTheme from "./tema_oscuro.js";
 
 const d = document;
 
@@ -12,10 +13,13 @@ d.addEventListener("DOMContentLoaded", e=>{//representa la carga del documento
     alarm("../assets/alarma-guerra.mp3","#activar-alarma","#desactivar-alarma");
     countDown("countdown","Jan 26, 2022 13:18:19","Feliz CUmpleaños amigo");
     scrollTopButton(".scroll-top-btn");
+    
 })
 
 d.addEventListener("keydown", e=>{
     //shortcuts(e);
     moveBall(e,".ball",".stage");
 })
+
+darkTheme(".dark-theme-btn","dark-mode");
 
---
+++ DOM/js/tema_oscuro.js
@@ -0,0 +1,52 @@
+//clase 89 90
+const d = document;
+const ls = localStorage;
+
+export default  function darkTheme(btn, classDark){
+    const $themeBtn = d.querySelector(btn),
+     $selectors = d.querySelectorAll("[data-dark]");
+     //console.log($selectors);
+
+    let moon = "🌙",
+        sun = "☀️";
+
+    function lightMode() {
+        $selectors.forEach(el => {
+            el.classList.remove(classDark);
+        })
+        $themeBtn.textContent = moon;
+        ls.setItem("theme","light")
+    };
+    function darkMode() {
+        $selectors.forEach(el => {
+            el.classList.add(classDark);
+        })
+        $themeBtn.textContent = sun;
+        ls.setItem("theme","dark")
+    };
+
+    d.addEventListener("click", (e) => {
+        if(e.target.matches(btn)){
+            //console.log($themeBtn.textContent)
+            if($themeBtn.textContent === moon){
+                darkMode();
+            }else{
+               lightMode();
+            }
+        }
+    })
+
+    d.addEventListener("DOMContentLoaded", (e) =>{
+        //api localstorage : me permite almacenar valores en los navegadores
+        console.log(ls.getItem("theme"))
+        if(ls.getItem("theme") === null){
+            ls.setItem("theme","light");
+        }
+        if(ls.getItem("theme") === "light"){
+            lightMode();
+        }
+        if(ls.getItem("theme") === "dark"){
+            darkMode();
+        }
+    })
+}
