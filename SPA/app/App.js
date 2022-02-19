9c1dd404c2aedcbc8a205b6394613c74f352e6d2
Diego <diego.sk75.dgp@gmail.com>
2022-02-18 20:05:58 -0500

    clase 147 148 149 150

--- SPA/app/App.js
+++ SPA/app/App.js
@@ -1,3 +1,29 @@
+import api from "./helpers/wp_api.js";
+import {ajax} from "./helpers/ajax.js";
+import {Title} from "./components/Title.js";
+import {Loader} from "./components/Loader.js";
+import { Header } from "./components/Header.js";
+
 export function App(){
-    document.getElementById("root").innerHTML = `<h1>Bienvenido a mi primer SPA con vanilla JS</h1>`;
+    //document.getElementById("root").innerHTML = `<h1>Bienvenido a mi primer SPA con vanilla JS</h1>`;
+    /* ajax({
+        url:api.POSTS,
+        cbSuccess: posts => {
+            console.log(posts);
+        }
+    })
+    ajax({
+        url:api.CATEGORIES,
+        cbSuccess: categories => {
+            console.log(categories);
+        }
+    }) */
+
+    const d = document,
+    $root = d.getElementById("root");
+
+    $root.appendChild(Header());
+    $root.appendChild(Loader());
+
+
 }
