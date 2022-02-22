9ec04b64ceef14d544a442033ee272a31edc04dd
Diego <diego.sk75.dgp@gmail.com>
2022-02-21 19:57:34 -0500

    clase 152 153 154

--- SPA/app/App.js
+++ SPA/app/App.js
@@ -1,45 +1,18 @@
-import api from "./helpers/wp_api.js";
-import {ajax} from "./helpers/ajax.js";
-import {Title} from "./components/Title.js";
+
 import {Loader} from "./components/Loader.js";
 import { Header } from "./components/Header.js";
-import { Posts } from "./components/Posts.js";
-import { PostCard } from "./components/PostCard.js";
+import { Main } from "./components/Main.js";
+
+import { Router } from "./components/Router.js";
 
 export function App(){
-    //document.getElementById("root").innerHTML = `<h1>Bienvenido a mi primer SPA con vanilla JS</h1>`;
-    /* ajax({
-        url:api.POSTS,
-        cbSuccess: posts => {
-            console.log(posts);
-        }
-    })
-    ajax({
-        url:api.CATEGORIES,
-        cbSuccess: categories => {
-            console.log(categories);
-        }
-    }) */
+   
+    const $root = document.getElementById("root");
 
-    const d = document,
-    $root = d.getElementById("root");
-
+    $root.innerHTML = null;
     $root.appendChild(Header());
-    $root.appendChild(Posts());
+    $root.appendChild(Main());
     $root.appendChild(Loader());
     
-    ajax({
-        url:api.POSTS,
-        cbSuccess:(posts)=>{
-            console.log(posts);
-            let html = "";
-            posts.forEach(post => { 
-                html += PostCard(post);
-            });
-            d.querySelector(".loader").style.display = "none";
-            d.getElementById("posts").innerHTML = html;
-        },
-    })
-
-
+    Router();
 }
